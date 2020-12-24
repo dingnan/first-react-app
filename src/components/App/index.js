35164e9d3e3f1d466b9f9111593f9a5a52d330b4
dingnan <nandingb@gmail.com>
2020-12-24 08:59:39 -0500

    add home and contact link to the header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "../Main";
+import {Link} from "react-router-dom";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <p>
+            <Link className="link" to="/">Home</Link>
+          </p>
           <p>TV Series List</p>
-        </header>        
+          <p>
+            <Link className="link" to="/contact">Contact</Link>
+          </p>
+        </header>
         <Main />
       </div>
     );
